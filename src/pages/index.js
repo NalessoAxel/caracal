b0b29726ab7679ac4ecbafd40153f89c73435c4f
NalessoAxel <axelnalesso@gmail.com>
2021-04-19 16:17:31 +0200

    added logo section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import { Heading, Text, Button, Flex, Box } from '@chakra-ui/react';
+import { Heading, Text, Button, Flex, Box, Color } from '@chakra-ui/react';
 
 import Container from '_comps/Layout/Container';
 import Col from '_comps/Layout/Col';
@@ -75,12 +75,29 @@ const Home = () => (
 		
 	</Container>
 	<Container>
-	<Col colStart={2} colEnd={4}>
-		
-			<Text color="#4377DA">
-				Ils nous font confiance
+	<Col colStart={2} colEnd={26}>
+		<Box>
+			<Flex 
+			align="center"
+			
+			justifyContent="space-between"
+			
+			
+			>
+			<Text 
+				color="#4377DA"
+				borderBottom="2px"
+				borderBottomWidth="3px"
+				my="100px">
+				Ils nous font <br /> confiance
 			</Text>
-		
+			<Image src="/Roularta.png" alt="" width="268" height="62" />
+			<Image src="/Alpha.png" alt="" width="221" height="54" />
+			<Image src="/caterpil.png" alt="" width="222" height="36" />
+			<Image src="/Orange.png" alt="" width="66" height="66" />
+			<Image src="/Worldine.png" alt="" width="144" height="26" />
+			</Flex>
+			</Box>
 		</Col>
 		</Container>
 	</>
